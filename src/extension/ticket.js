18e58a5a7a54377b2d2305f9faf95ac688b9220e
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-05-11 09:43:28 +0100

    Configure routing for different ticket types
    This allows us to be able to apply different scripts to tickets
    depending on if they're existing and being viewed or edited, or are
    completely new.

--- src/extension/ticket.js
+++ src/extension/ticket.js
@@ -1,4 +1,41 @@
-console.log('This is autotasks ticket detail script!')
+let ticketUrls = {
+  new: 'https://ww16.autotask.net/autotask/views/ServiceDesk/ServiceDeskTicket/service_ticket_new.aspx',
+  existing: 'https://ww16.autotask.net/autotask/views/ServiceDesk/ServiceDeskTicket/service_ticket.aspx'
+}
 
-// TODO: Display names in Firstname, Lastname format
-// TODO: Select the logged in user as the primary resource for new tickets
+// we have to inject this code, since chrome extensions can't access
+// the real window variable and thus can't access the ticket's mode
+let codeToInject = `
+<script type="text/javascript">
+  var atTicketEvent = new CustomEvent(
+    'ATTicketEvent',
+    {detail: window.serviceTicket._mode}
+  );
+  document.head.dispatchEvent(atTicketEvent);
+</script>
+`
+
+// apply a script depending on whether it's being edited or viewed
+let routeExistingTicket = () => {
+  $(document.head).on('ATTicketEvent', e => {
+    let ticketMode = e.detail
+
+    if (ticketMode === 0) {
+      console.log('Do view ticket things')
+    } else if (ticketMode === 1) {
+      console.log('Do edit ticket things')
+    }
+  })
+
+  // wait until load finishes, otherwise window.serviceTicket
+  // won't have been defined yet
+  $('#bodyIframe').on('load', () => {
+    $(document.head).append(codeToInject)
+  })
+}
+
+if (RegExp(ticketUrls.new, 'g').test(location.href)) {
+  console.log('Do new ticket things')
+} else if (RegExp(ticketUrls.existing, 'g').test(location.href)) {
+  routeExistingTicket()
+}
